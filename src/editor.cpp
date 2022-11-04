6543ef431faae9d38f775efd315dd08c8e724ede
Ahmet Kerem <akerem14@gmail.com>
2022-11-04 13:18:11 +0000

    test

--- src/editor.cpp
+++ src/editor.cpp
@@ -1,11 +1,13 @@
 #include <editor.h>
 
+#include <glad/glad.h>
+
 Editor::Editor(): App{"Wans", 800, 1000}
 {
-
+    glClearColor(0,1,0,1);
 }
 
 void Editor::render()
 {
-
+    glClear(GL_COLOR_BUFFER_BIT);
 }
