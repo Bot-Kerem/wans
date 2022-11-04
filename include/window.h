8d8c6a887fb2ce181dfc9701cb476ee3725b8196
Ahmet Kerem <akerem14@gmail.com>
2022-11-04 12:32:29 +0000

    changes

---
+++ include/window.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+#include <string>
+
+typedef GLFWwindow* IWindow;
+
+class Window
+{
+    private:
+            std::string m_Title = 0;
+            int m_Width = 0;
+            int m_Height = 0;
+            int m_ViewportWidth = 0;
+            int m_ViewportHeight = 0;
+            double m_MousePosX = 0;
+            double m_MousePosY = 0;
+
+            IWindow m_Window;
+
+            static void _FramebufferSizeCallback(IWindow _Window, int Width, int Height) noexcept;
+            static void _WindowSizeCallback(IWindow _Window, int Width, int Height) noexcept;
+
+            void (*FramebufferSizeCallback)(int, int) = nullptr;
+            void (*WindowSizeCallback)(int, int) = nullptr;
+    public:
+        Window(std::string Title, int Width, int Height);
+
+        void pollEvents() noexcept;
+
+        // 
+        inline int getWidth() const noexcept { return m_Width; }
+        inline int getHeight() const noexcept { return m_Height; }
+        inline IWindow getWindow() const noexcept { return m_Window; }
+        inline int getViewportWidth() const noexcept { return m_ViewportWidth; }
+        inline int getViewportHeight() const noexcept { return m_ViewportHeight; }
+        inline int isClosed() const noexcept { return glfwWindowShouldClose(m_Window); }
+        inline int getTitle() const noexcept { return m_Title; }
+
+
+        void setFramebufferSizeCallback(void (*fn)(int, int)) noexcept;
+        void setWindowSizeCallback(void (*fn)(int, int)) noexcept;
+
+        // removing copy constructors
+        Window(const Window&) = delete;
+        Window operator=(const Window&) = delete;
+
+};
---
+++ src/window.cpp
@@ -0,0 +1,73 @@
+#include <window.h>
+
+#ifndef GL_VERSION_MAJOR
+    #define GL_VERSION_MAJOR 4
+#endif
+#ifndef GL_VERSION_MINOR
+    #define GL_VERSION_MINOR 6
+#endif
+#ifndef GL_PROFILE
+    #define GL_PROFILE GLFW_OPENGL_COMPAT_PROFILE
+#endif
+
+Window::Window(std::string Title, int Width, int Height)
+    : m_Title{Title}, m_Width{Width}, m_Height{Height}
+{
+    // Initializing GLFW
+    glfwInit();
+
+    // Setting OpenGL Context Version 4.6
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_VERSION_MAJOR);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_VERSION_MINOR);
+    // Setting OpenGL profile to compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GL_PROFILE);
+
+    m_Window = glfwCreateWindow(Width, Height, Title.c_str(), nullptr, nullptr);
+
+    glfwMakeContextCurrent(m_Window);
+    // TODOL: load gl
+
+    glfwSetWindowUserPointer(m_Window, this);
+}
+
+void Window::pollEvents() noexcept
+{
+    glfwPollEvents();
+    glfwGetWindowSize(m_Window, &m_Width, &m_Height);
+    glfwGetCursorPos(m_Window, &m_MousePosX, &m_MousePosY);
+    auto a = [](int){return 1;};
+}
+
+void Window::setFramebufferSizeCallback(void (*fn)(int, int)) noexcept
+{
+    glfwSetFramebufferSizeCallback(m_Window, _FramebufferSizeCallback);
+    FramebufferSizeCallback = fn;
+}
+
+void Window::setWindowSizeCallback(void (*fn)(int, int)) noexcept
+{
+    glfwSetWindowSizeCallback(m_Window, _WindowSizeCallback);
+    WindowSizeCallback = fn;
+}
+
+void Window::_FramebufferSizeCallback(IWindow _Window, int Width, int Height) noexcept
+{
+    auto window = static_cast<Window*>(glfwGetWindowUserPointer(_Window));
+
+    if(window->FramebufferSizeCallback)
+    {
+        window->FramebufferSizeCallback(Width, Height);
+    }
+}
+
+void Window::_WindowSizeCallback(IWindow _Window, int Width, int Height) noexcept
+{
+    auto window = static_cast<Window*>(glfwGetWindowUserPointer(_Window));
+
+    if(window->WindowSizeCallback)
+    {
+        window->WindowSizeCallback(Width, Height);
+    }
+}
+
+
