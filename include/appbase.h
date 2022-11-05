d2e30ed98850be13ba3684498acdec8c9d26baa7
Ahmet Kerem <akerem14@gmail.com>
2022-11-05 12:06:03 +0000

    update theme

--- include/appbase.h
+++ include/appbase.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <window.h>
+#include <theme.h>
 
 class AppBase
 {
     protected:
         Window m_Window;
+        Theme theme{};
 
         virtual void render() = 0;
         AppBase(std::string Title, int Width, int Height);
--- src/editor.cpp
+++ src/editor.cpp
@@ -6,8 +6,8 @@
 
 Editor::Editor(): AppBase{"Wans", 800, 1000}
 {
-    glClearColor(0,1,0,1);
-    m_Window.setFramebufferSizeCallback(std::bind(&Editor::c, this, std::placeholders::_1, std::placeholders::_2));
+    glClearColor(theme[ThemeCol_Background].r, theme[ThemeCol_Background].g, theme[ThemeCol_Background].b, 1);
+    //m_Window.setFramebufferSizeCallback(std::bind(&Editor::c, this, std::placeholders::_1, std::placeholders::_2));
 }
 
 void Editor::render()
