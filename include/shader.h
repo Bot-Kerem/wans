3d0c20fd01a8c1d13791e992f7c318d5e6ca2b63
Ahmet Kerem <akerem14@gmail.com>
2022-11-06 18:18:42 +0000

    added setting uniform var

--- include/shader.h
+++ include/shader.h
@@ -3,6 +3,9 @@
 #include <string>
 
 #include <glad/glad.h>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
 
 class Shader
 {
@@ -10,8 +13,13 @@ class Shader
         unsigned int m_Program;
         void Compile(unsigned int Shader) const noexcept;
         void Link() const noexcept;
+        inline int getUniformLocation(const char* name) const noexcept { return glGetUniformLocation(m_Program, name); }
     public:
         Shader(std::string VertexShaderPath, std::string FragmentShaderPath) noexcept;
+
+        inline void setVec2(const char* name, const glm::vec2& val) const noexcept { glUniform2f(getUniformLocation(name), val.x, val.y); }
+        inline void setVec3(const char* name, const glm::vec3& val) const noexcept { glUniform3f(getUniformLocation(name), val.x, val.y, val.z); }
+        inline void setVec4(const char* name, const glm::vec4& val) const noexcept { glUniform4f(getUniformLocation(name), val.r, val.g, val.b, val.a); }
 
         inline void use() const noexcept
         {
