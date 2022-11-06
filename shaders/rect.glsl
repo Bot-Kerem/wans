bc859f7780855997be27dcca47a0e6e9acf1e4a1
Ahmet Kerem <akerem14@gmail.com>
2022-11-06 11:14:59 +0000

    rename

--- shaders/rect.glsl
+++ shaders/rect.glsl
@@ -7,17 +7,7 @@ vec2 rect[4] = vec2[](
     vec2( 1.0f, -1.0f)
 );
 
-vec2 tex[4] = vec2[](
-    vec2(0.0f, 1.0f),
-    vec2(0.0f, 0.0f),
-    vec2(1.0f, 1.0f),
-    vec2(1.0f, 0.0f)
-);
-
-out vec2 TextureCoord;
-
 void main()
 {
     gl_Position = vec4(rect[gl_VertexID], 0.0f, 1.0f);
-    TextureCoord = tex[gl_VertexID]
 }
