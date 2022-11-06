bd08d0464214d2ea48e99cb270496e190ace2418
Ahmet Kerem <akerem14@gmail.com>
2022-11-06 11:19:39 +0000

    added shader

--- include/widget.h
+++ include/widget.h
@@ -3,6 +3,7 @@
 #include <framebuffer.h>
 #include <memory>
 #include <vector>
+#include <shader.h>
 
 class Widget
 {
@@ -13,7 +14,12 @@ class Widget
         std::unique_ptr<Framebuffer> m_Framebuffer;
         float m_Width;
         float m_Height;
+
+        inline static Shader* RectShader = nullptr;
+        inline static Shader* TextureShader = nullptr;
     public:
+        static void init() noexcept;
+        static void destroy() noexcept;
         Widget(float Width, float Height) noexcept;
         void draw() noexcept;
         void update() noexcept;
