e4c9a2d7f5f1324fa5936f6a8c79af5c8ccfa299
Ahmet Kerem <akerem14@gmail.com>
2022-11-06 10:50:39 +0000

    removed theme

--- include/appbase.h
+++ include/appbase.h
@@ -1,13 +1,11 @@
 #pragma once
 
 #include <window.h>
-#include <theme.h>
 
 class AppBase
 {
     protected:
         Window m_Window;
-        Theme theme{};
 
         virtual void render() = 0;
         AppBase(std::string Title, int Width, int Height);
