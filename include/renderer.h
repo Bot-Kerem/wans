93f808ad3c2632c9b8a2ddc617b8be036afa129a
Ahmet Kerem <akerem14@gmail.com>
2022-11-06 11:19:26 +0000

    added set viewport size func

--- include/renderer.h
+++ include/renderer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <glad/glad.h>
+#include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
 
@@ -10,18 +11,31 @@ namespace Renderer
     {
         glClearColor(r, g, b, a);
     }
+
     inline void Clear() noexcept
     {
         glClear(GL_COLOR_BUFFER_BIT);
     }
+
     inline void Clear(glm::vec3 Color, float Alpha = 1.0f) noexcept
     {
         SetClearColor(Color.r, Color.g, Color.b, Alpha);
         Clear();
     }
+
     inline void Clear(glm::vec4 Color) noexcept
     {
         SetClearColor(Color.r, Color.g, Color.b, Color.a);
         Clear();
+    }
+
+    inline void SetViewport(int Width, int Height) noexcept
+    {
+        glViewport(0, 0, Width, Height);
+    }
+    
+    inline void SetViewport(glm::ivec2 size) noexcept
+    {
+        glViewport(0, 0, size.x, size.y);
     }
 } // namespace Renderer
