f6b9d128fc51fcd4f3e491bbbef0765d56798f8e
Ahmet Kerem <akerem14@gmail.com>
2022-11-06 11:00:43 +0000

    added renderer

---
+++ include/renderer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+namespace Renderer
+{
+    inline void SetClearColor(float r, float g, float b, float a) noexcept
+    {
+        glClearColor(r, g, b, a);
+    }
+    inline void Clear() noexcept
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+    inline void Clear(glm::vec3 Color, float Alpha = 1.0f) noexcept
+    {
+        SetClearColor(Color.r, Color.g, Color.b, Alpha);
+        Clear();
+    }
+    inline void Clear(glm::vec4 Color) noexcept
+    {
+        SetClearColor(Color.r, Color.g, Color.b, Color.a);
+        Clear();
+    }
+} // namespace Renderer
