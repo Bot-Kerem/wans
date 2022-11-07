03d182f168d52031d20c2d4ae22753eccc46b65c
Bot-Kerem <akerem1434@gmail.com>
2022-11-07 07:07:19 +0000

    ?

--- include/mainviewport.h
+++ include/mainviewport.h
@@ -7,5 +7,5 @@ class MainViewport: public Widget
     private:
     public:
         MainViewport(float Width, float Height);
-        void create_image() const noexcept override; 
+        void create_image() const noexcept override;
 };
