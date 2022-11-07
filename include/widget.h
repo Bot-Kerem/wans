998d3235b5da1f0fff67afdb3338e4517c1a20fa
Bot-Kerem <akerem1434@gmail.com>
2022-11-07 14:35:32 +0000

    widgets

--- include/widget.h
+++ include/widget.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <framebuffer.h>
+#include <shader.h>
 #include <memory>
 #include <vector>
-#include <shader.h>
+#include <glm/vec2.hpp>
 
 class Widget
 {
@@ -14,17 +15,23 @@ class Widget
         std::unique_ptr<Framebuffer> m_Framebuffer;
         float m_Width;
         float m_Height;
+        glm::vec2 m_Position{0.0f};
 
         inline static Shader* RectShader = nullptr;
         inline static Shader* TextureShader = nullptr;
     public:
+        inline void addChild(Widget* widget) noexcept
+        {
+            m_Childs.push_back(widget);
+            widget->m_Parent = this;
+        }
         virtual void setSize(float Width, float Height) noexcept
         {
             m_Width = Width;
             m_Height = Height;
-            for(auto& widget: m_Childs)
+            for(Widget* widget: m_Childs)
             {
-                setSize(Width, Height);
+                widget->setSize(Width, Height);
             }
             update();
         }
--- src/widget.cpp
+++ src/widget.cpp
@@ -21,24 +21,31 @@ Widget::Widget(float Width, float Height) noexcept
 
 void Widget::draw() noexcept
 {
-    Renderer::SetViewport(m_Width, m_Height);
     TextureShader->use();
     m_Framebuffer->setTexture();
+    if(m_Parent)
+    {
+        TextureShader->setVec2("size", {static_cast<float>(m_Width)/static_cast<float>(m_Parent->m_Width), static_cast<float>(m_Height)/static_cast<float>(m_Parent->m_Height)});
+        TextureShader->setVec2("pos", m_Position/glm::vec2{m_Parent->m_Width,m_Parent->m_Height});
+        Renderer::SetViewport(m_Parent->m_Width, m_Parent->m_Height);
+    }
+    else
+    {
+        Renderer::SetViewport(m_Width, m_Height);
+    }
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
 void Widget::update() noexcept
 {
     // create_image();
+    Renderer::SetViewport(m_Width, m_Height);
+    m_Framebuffer->begin();
+    create_image();
     for(Widget* widget: m_Childs)
     {
         widget->draw();
     }
-
-    Renderer::SetViewport(m_Width, m_Height);
-    m_Framebuffer->begin();
-    create_image();
     m_Framebuffer->end();
-
     if(m_Parent) m_Parent->update();
 }
