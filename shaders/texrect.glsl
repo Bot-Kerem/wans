3354b4ca0cc543c7857e58884c326641bc01e050
Bot-Kerem <akerem1434@gmail.com>
2022-11-07 14:35:46 +0000

    added pos and size

--- shaders/texrect.glsl
+++ shaders/texrect.glsl
@@ -16,8 +16,11 @@ vec2 tex[4] = vec2[](
 
 out vec2 TextureCoord;
 
+uniform vec2 size = vec2(1.0f, 1.0f);
+uniform vec2 pos = vec2(0.0f, 0.0f);
+
 void main()
 {
-    gl_Position = vec4(rect[gl_VertexID], 0.0f, 1.0f);
+    gl_Position = vec4(rect[gl_VertexID] * size + pos, 0.0f, 1.0f);
     TextureCoord = tex[gl_VertexID];
 }
