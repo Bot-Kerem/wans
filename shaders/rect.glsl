e7cb7e4ecf27331a6059ad8f3c9d797c53fbee01
Bot-Kerem <akerem1434@gmail.com>
2022-11-07 07:10:44 +0000

    added size and pos

--- shaders/rect.glsl
+++ shaders/rect.glsl
@@ -7,7 +7,10 @@ vec2 rect[4] = vec2[](
     vec2( 1.0f, -1.0f)
 );
 
+uniform vec2 size;
+uniform vec2 pos;
+
 void main()
 {
-    gl_Position = vec4(rect[gl_VertexID], 0.0f, 1.0f);
+    gl_Position = vec4(rect[gl_VertexID] * size + pos, 0.0f, 1.0f);
 }
